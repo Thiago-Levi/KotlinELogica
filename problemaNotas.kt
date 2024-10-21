f85b68935a8af3d96f850e8ec97c5d576c85710a
Levi's <110788808+Thiago-Levi@users.noreply.github.com>
2024-10-21 16:20:15 -0300

    Add files via upload

---
+++ problemaNotas.kt
@@ -0,0 +1,11 @@
+fun main() {
+    fun scoreProblem(score1: Double, score2: Double): String{
+        val finalScore = (score1 + score2) / 2
+        val passOrFail = if(finalScore < 60.0) "Reprovado" else "Aprovado"
+
+        return "Nota final: $finalScore\nResultado: $passOrFail"
+    }
+
+    val output = scoreProblem(45.5, 31.30)
+    println(output)
+}
