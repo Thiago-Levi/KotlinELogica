42d92d37ce54fdf36469d8ef306589f1749e9cd4
Levi's <110788808+Thiago-Levi@users.noreply.github.com>
2024-11-14 16:38:25 -0300

    Add files via upload

--- ProblemaLanchonete.kt
+++ ProblemaLanchonete.kt
@@ -2,15 +2,14 @@ fun main(){
     fun fastfoodProblem(code: Int): String{
         var price = 0.0
 
-        when(code){
-            1 -> price = 5.00
-            2 -> price = 3.50
-            3 -> price = 4.80
-            4 -> price = 8.90
-            5 -> price = 4.32
+        price = when(code){
+            1 -> 5.00
+            2 -> 3.50
+            3 -> 4.80
+            4 -> 8.90
+            5 -> 4.32
             else -> return "Código inválido"
         }
-
         return "Código: $code - Valor: ${String.format("%.2f", price)}"
     }
 
